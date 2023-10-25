d9629551ac5b6c0e39ad89d3dab38c7b70b1c5fe
graviolagames <graviolagames@gmail.com>
2023-10-25 17:02:27 -0300

    Implement create operation on User_DAO

--- backend/api/DAO/user_dao.py
+++ backend/api/DAO/user_dao.py
@@ -1,5 +1,6 @@
 from database_client import dynamo
 from definitions import return_values
+from util import data_util
 
 # User_DAO Manager User Crup operations.
 class User_DAO:
@@ -7,6 +8,14 @@ class User_DAO:
         self.table_name = user_table_name
         self.db_instance = dynamo.Dynamo_instance()
     
+    def validate_user(self, user): 
+        if 'name' not in user or 'password' not in user :
+            return False
+        if not isinstance(user['name'], str) or not isinstance(user['password'], str):
+            return False
+        return True
+
+
     # create the table for users
     # return values:
     # TABLE_ALREADY_EXISTS
@@ -31,15 +40,28 @@ class User_DAO:
         self.db_instance.client.create_table(**table_params)
         return dynamo.wait_table_creation(self.table_name)
         
-    """
-    #Creat an user 
+    
+    #Create an user 
     # return values:
     # TABLE_NOT_FOUND
     # TIME_OUT
     # SUCCESS
-    def create_user():
+    def create_user(self,user_param):
         if not dynamo.check_table_existence(self.table_name):
             return return_values.TABLE_NOT_FOUND
+        if not self.validate_user(user_param):
+            return return_values.INVALID_INPUT_DATA
+        id = data_util.create_hash(user_param['name'])
+        item = {
+            'id':{'S':id},
+            'name':{'S':user_param['name']},
+            'password':{'S':user_param['password']}
+        }
+        self.db_instance.client.put_item(
+            TableName = self.table_name,
+            Item = item
+        )
+        return id
         
-     """
+     
     
--- backend/api/tests/test_user_dao.py
+++ backend/api/tests/test_user_dao.py
@@ -2,63 +2,75 @@ import pytest
 from DAO import user_dao as dao
 import boto3
 import time
-user_table_name = "TestUserTable"
 
-class TestUserController:
+class TestUserDAO:
     _SLEEP = 10
-    _dao = dao.User_DAO(user_table_name)
+    _user_table_name = "TestUserTable"
+    _dao = None
 
-    def _delete_table(self):
-        print('_delete_table')
+    @classmethod
+    def _delete_table(cls):
+        print('Entering _delete_table')
         dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table(user_table_name)
-        if table != None:
+        table = dynamodb.Table(cls._user_table_name)
+        if table:
             table.delete()
             print("Table deleted")
-            time.sleep(self._SLEEP)
+            time.sleep(cls._SLEEP)
+        print('Table was not deleted (Not found)')
 
-    def _get_table(self):
+    @classmethod
+    def _get_table(cls):
         print('Entering _get_table')
         dynamodb = boto3.resource("dynamodb")
-        return dynamodb.Table(user_table_name)
-        
-    def _get_table_status(self):
-        print('Entering _get_table_status')
-        table = self._get_table()
-        return table.table_status
+        response =  dynamodb.Table(cls._user_table_name)
+        return response
 
-    def _get_table_item(self,item_id):
+    @classmethod    
+    def _get_table_status(cls):
+        print('Entering _get_table_status')
+        table = cls._get_table()
+        if table:
+            return table.table_status
+        return return_values.TABLE_NOT_FOUND
+
+    @classmethod
+    def _get_table_item(cls,item_id):
         print('Entering _get_table_item')
-        table = _get_table()
-        return table.get_item(
-            key={
-                'id':item_id
-            }
-        )
+        table = cls._get_table()
+        if table:
+            return table.get_item(
+                Key={
+                    'id':item_id
+                }
+            )
+        return return_values.TABLE_NOT_FOUND
+        
     
     #User_DAO must create a table that should be accessible by boto3
-    @pytest.fixture(scope='module', autouse=True)
-    def setup_module_fixture(self):
-        print('Entering setup_module_fixture')
+    def setup_class(self):
+        self._dao = dao.User_DAO(self._user_table_name)
+        print('Entering setup_class')
         ret_value = self._dao.create_user_table()
         print('create_user_table: '+ ret_value)
-        assert self._get_table_status() == "ACTIVE"
+        table_status = self._get_table_status() 
+        assert table_status == "ACTIVE"
     
-    @pytest.fixture(scope='module', autouse=True)
-    def teardown_module(self):
-        print('Entering teardown_module')
+        
+    #User_DAO must create item on user table
+    def test_create_user(self):
+        print('Entering test_create_user')
+        user_param = {'name': 'Dino da Silva Sauro',
+                      'password': '123456'}
+        id = self._dao.create_user(user_param)
+        print('expected item id as response. Received ' + id)
+        time.sleep(self._SLEEP)
+        response = self._get_table_item(id)
+        response_item = response['Item']
+        assert response_item["name"] == user_param["name"]
+        assert response_item["password"] == user_param["password"]
+    
+    def teardown_class(self):
+        print('Entering teardown_class')
         self._delete_table()
 
-    def test_dummy(self):
-        print('Entering test_dummy')
-
-    """
-    #User_DAO must create item on user table
-    def test_create_user():
-        user_param = {'name': 'Dino da Silva Sauro',
-                      'senha': '123456'}
-        userid = self._dao.create_user(user_param)
-        time.sleep(self._SLEEP)
-        assert self._get_table_item(user_id) == user_param
-    """
-        
