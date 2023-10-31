b04084dd82847d35bcde107f4652bb666f6f1c6f
graviolagames <graviolagames@gmail.com>
2023-10-31 15:10:31 -0300

    Create asset_dao

---
+++ backend/api/DAO/asset_dao.py
@@ -0,0 +1,144 @@
+from database_client import dynamo
+from definitions import return_values
+from util import data_util
+
+# Asset_DAO Manager Asset Crud operations.
+class Asset_DAO:
+    def __init__(self,asset_table_name):
+        self.table_name = asset_table_name
+        self.db_instance = dynamo.Dynamo_instance()
+    
+    def validate_asset(self, asset):
+        required_fields = ['name','description','url'] 
+        if not all(field in asset for field in required_fields):
+            return False
+        if not all(isinstance(asset[field], str) for field in required_fields):
+            return False
+        return True
+
+
+    # create the table for assets
+    # return values:
+    # TABLE_ALREADY_EXISTS
+    # TIME_OUT
+    # SUCCESS
+    def create_asset_table(self):
+        if dynamo.check_table_existence(self.table_name):
+            return return_values.TABLE_ALREADY_EXISTS
+        table_params = {
+            'TableName': self.table_name,
+            'KeySchema': [
+                {'AttributeName': 'id', 'KeyType': 'HASH'}
+            ],
+            'AttributeDefinitions': [
+                {'AttributeName': 'id', 'AttributeType': 'S'}
+            ],
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            } 
+        }
+        try:
+            self.db_instance.client.create_table(**table_params)
+        except Exception as e:
+            return str(e)
+        return dynamo.wait_table_creation(self.table_name)
+        
+    
+    #Create an asset 
+    # return values:
+    # TABLE_NOT_FOUND
+    # TIME_OUT
+    # SUCCESS
+    def create_asset(self,asset_param):
+        if not dynamo.check_table_existence(self.table_name):
+            return return_values.TABLE_NOT_FOUND
+        if not self.validate_asset(asset_param):
+            return return_values.INVALID_INPUT_DATA
+        id = data_util.create_hash(asset_param['name'])
+        item = {
+            'id':{'S':id},
+            'name':{'S':asset_param['name']},
+            'description':{'S':asset_param['description']},
+            'url':{'S':asset_param['url']}
+        }
+        try:
+            self.db_instance.client.put_item(
+                TableName = self.table_name,
+                Item = item
+            )
+        except Exception as e:
+            return str(e)
+        return id
+        
+    # Read an asset 
+    # return values:
+    # The asset data
+    # TABLE_NOT_FOUND
+    # INVALID_INPUT_DATA
+    def read_asset(self,asset_id):
+        if not dynamo.check_table_existence(self.table_name):
+            return return_values.TABLE_NOT_FOUND
+        if not asset_id:
+            return return_values.INVALID_INPUT_DATA
+        try:
+            response = self.db_instance.client.get_item(
+                    TableName = self.table_name,
+                    Key = {'id': {'S': asset_id}}
+            )
+            if 'Item' in response:
+                return response['Item']
+            else:
+                return return_values.asset_NOT_FOUND
+        except Exception as e:
+            return str(e)    
+
+    # Update  asset 
+    # return values:
+    # TABLE_NOT_FOUND
+    # INVALID_INPUT_DATA
+    # asset_NOT_FOUND
+    # SUCCESS
+    def update_asset(self,asset_id,asset_param):
+        if not dynamo.check_table_existence(self.table_name):
+            return return_values.TABLE_NOT_FOUND
+        if not self.validate_asset(asset_param) or not asset_id:
+            return return_values.INVALID_INPUT_DATA
+    
+        try:
+            update_expression = "SET #n = :new_name, #d = :new_description,#u = :new_url"
+            expression_attribute_names = {"#n": "name", "#d": "description", "#u":"url"}
+            expression_attribute_values = {
+                ":new_name": {"S": asset_param['name']},
+                ":new_description": {"S": asset_param['description']},
+                ":new_url": {"S": asset_param['url']},
+            }
+            response = self.db_instance.client.update_item(
+                TableName=self.table_name,
+                Key={"id": {"S": asset_id}},
+                UpdateExpression=update_expression,
+                ExpressionAttributeNames=expression_attribute_names,
+                ExpressionAttributeValues=expression_attribute_values,
+                ReturnValues=return_values.SUCCESS
+            )
+        except Exception as e:
+            return return_values.ERROR + str(e)
+
+    # delete an asset 
+    # return values:
+    # TABLE_NOT_FOUND
+    # INVALID_INPUT_DATA
+    # SUCCESS
+    def delete_asset(self,asset_id):
+        if not dynamo.check_table_existence(self.table_name):
+            return return_values.TABLE_NOT_FOUND
+        if not asset_id:
+            return return_values.INVALID_INPUT_DATA
+        try:
+            response = self.db_instance.client.delete_item(
+                    TableName = self.table_name,
+                    Key = {'id': {'S': asset_id}}
+            )
+            return return_values.SUCCESS
+        except Exception as e:
+            return str(e)    
